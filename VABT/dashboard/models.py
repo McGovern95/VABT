20f25499d9fa0c6adcd724f18831fdd176df705f
McGovern95 <mcgovernchristian@gmail.com>
2019-03-21 11:31:19 -0600

    added baseline models for files

--- VABT/dashboard/models.py
+++ VABT/dashboard/models.py
@@ -17,12 +17,6 @@ class Post(models.Model):
     def get_absolute_url(self):
         return reverse('post-detail',kwargs={'pk':self.pk})
 
-#possible model for doucuments uploaded
-class Document(models.Model):
-    description = models.CharField(max_length=255, blank=True)
-    document = models.FileField(upload_to='documents/')
-    uploaded_at = models.DateTimeField(auto_now_add=True)
-
 
 
         
--- VABT/dashboard/urls.py
+++ VABT/dashboard/urls.py
@@ -16,10 +16,9 @@ urlpatterns = [
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('student_home/', views.student_home,name='student-home'),
-    #path('certifier_home/', views.certifier_home, name='certifier-home'),
     path('about/', views.about, name='dashboard-about'),
     path('contact/', views.contact, name='dashboard-contact'),
-    #path('checklist/', views.checklist, name='dashboard-checklist'), not sure how this'll be for file uploading and stuff
+    
  
 
 ]
