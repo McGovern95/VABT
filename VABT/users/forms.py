3729488e60c84e3b8b87b147aa603148729bd1de
Joshua Rosencrans <rosencrans24@gmail.com>
2019-04-09 11:32:33 -0600

    Added logic for phone number notifications

--- VABT/users/forms.py
+++ VABT/users/forms.py
@@ -2,6 +2,7 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from .models import Profile
+from users.models import UserExtended
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
@@ -21,3 +22,13 @@ class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ['image']
+
+class UserExtendedRegisterForm(forms.ModelForm):
+    class Meta:
+        model = UserExtended
+        fields = ['phone_number','phone_notifications','carrier']
+
+class UserExtendedUpdateForm(forms.ModelForm):
+    class Meta:
+        model = UserExtended
+        fields = ['phone_number','phone_notifications','carrier']
